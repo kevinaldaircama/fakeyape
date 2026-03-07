d037ca3c0200ccae7d08674af18e54bd19c33c3c
kevinaldaircama <kevinaldaircamachoserna51@gmail.com>
2026-03-06 20:14:27 -0500

    Update envío.js

--- app/envío.js
+++ app/envío.js
@@ -1,21 +1,24 @@
-function enviar() {
+document.getElementById("continuarBtn").addEventListener("click", enviar);
 
-  const nombre = document.getElementById("nombre").value.trim();
-  const telefonoSeleccionado = document.getElementById("telefono").value.trim();
-  const monto = document.getElementById("monto").value.trim();
-  const destinoSeleccionado = document.getElementById("destino").value.trim();
+function enviar(){
 
-  if (!nombre || !telefonoSeleccionado || !monto || !destinoSeleccionado) {
-    alert("Por favor completa todos los campos");
-    return;
-  }
+const nombre=document.getElementById("nombre").value.trim();
+const telefono=document.getElementById("telefono").value.trim();
+const monto=document.getElementById("monto").value.trim();
+const destino=document.getElementById("destino").value.trim();
 
-  const url =
-    "comprobante1.html?" +
-    "nombre=" + encodeURIComponent(nombre) +
-    "&telefono=" + encodeURIComponent(telefonoSeleccionado) +
-    "&monto=" + encodeURIComponent(monto) +
-    "&destino=" + encodeURIComponent(destinoSeleccionado);
+if(!nombre || !telefono || !monto || !destino){
+alert("Por favor completa todos los campos");
+return;
+}
 
-  window.location.href = url;
+const url =
+"comprobante1.html?" +
+"nombre=" + encodeURIComponent(nombre) +
+"&telefono=" + encodeURIComponent(telefono) +
+"&monto=" + encodeURIComponent(monto) +
+"&destino=" + encodeURIComponent(destino);
+
+window.location.href = url;
+
 }
